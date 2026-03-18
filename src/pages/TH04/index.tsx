895990b68ba6ff09f2bbe50d2bea6551da50de6c
LE TRONG MAI <buncha453@gmail.com>
2026-03-18 10:21:45 +0700

    main

--- src/pages/TH04/index.tsx
+++ src/pages/TH04/index.tsx
@@ -7,27 +7,27 @@ import ThongTinVanBang from './ThongTinVanBang';
 import TraCuuVanBang from './TraCuuVanBang';
 
 const Bai4Main = () => {
-  // Gọi đúng tên model "localStorage" (tương ứng với tên file localStorage.ts)
-  const storageModel = useModel('localStorage');
+	// Gọi đúng tên model "localStorage" (tương ứng với tên file localStorage.ts)
+	const storageModel = useModel('TH04.localStorage');
 
-  return (
-    <PageContainer title="HỆ THỐNG QUẢN LÝ VĂN BẰNG (TH04) - PTIT">
-      <Tabs type="card" style={{ background: '#fff', padding: 16, borderRadius: 8 }}>
-        <Tabs.TabPane tab="1. Cấu hình biểu mẫu" key="1">
-          <CauHinhBieuMau {...storageModel} />
-        </Tabs.TabPane>
-        <Tabs.TabPane tab="2. Quyết định & Sổ" key="2">
-          <QuanLySoQuyetDinh {...storageModel} />
-        </Tabs.TabPane>
-        <Tabs.TabPane tab="3. Thông tin văn bằng" key="3">
-          <ThongTinVanBang {...storageModel} />
-        </Tabs.TabPane>
-        <Tabs.TabPane tab="4. Tra cứu văn bằng" key="4">
-          <TraCuuVanBang {...storageModel} />
-        </Tabs.TabPane>
-      </Tabs>
-    </PageContainer>
-  );
+	return (
+		<PageContainer title='HỆ THỐNG QUẢN LÝ VĂN BẰNG (TH04) - PTIT'>
+			<Tabs type='card' style={{ background: '#fff', padding: 16, borderRadius: 8 }}>
+				<Tabs.TabPane tab='1. Cấu hình biểu mẫu' key='1'>
+					<CauHinhBieuMau {...storageModel} />
+				</Tabs.TabPane>
+				<Tabs.TabPane tab='2. Quyết định & Sổ' key='2'>
+					<QuanLySoQuyetDinh {...storageModel} />
+				</Tabs.TabPane>
+				<Tabs.TabPane tab='3. Thông tin văn bằng' key='3'>
+					<ThongTinVanBang {...storageModel} />
+				</Tabs.TabPane>
+				<Tabs.TabPane tab='4. Tra cứu văn bằng' key='4'>
+					<TraCuuVanBang {...storageModel} />
+				</Tabs.TabPane>
+			</Tabs>
+		</PageContainer>
+	);
 };
 
 export default Bai4Main;
